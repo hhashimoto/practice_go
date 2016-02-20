08b6757ad0872bc1fdb5d5d960d7bf6a735d94f0
hhashimoto <almite.bridge@gmail.com>
2016-02-20 13:40:39 +0900

    '\t' => 4 spaces

--- chat/main.go
+++ chat/main.go
@@ -3,29 +3,29 @@ package main
 import(
     "log"
     "net/http"
-	"text/template"
-	"path/filepath"
-	"sync"
+    "text/template"
+    "path/filepath"
+    "sync"
 )
 
 // HTMLテンプレート
 type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
+    once     sync.Once
+    filename string
+    templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-	if err := t.templ.Execute(w, nil); err != nil {
-		panic(err)
-	}
+    t.once.Do(func() {
+        t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+    })
+    if err := t.templ.Execute(w, nil); err != nil {
+        panic(err)
+    }
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+    http.Handle("/", &templateHandler{filename: "chat.html"})
 
     // Start web server
     if err := http.ListenAndServe(":8080", nil); err != nil {
